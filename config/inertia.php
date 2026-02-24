0f6ccfaf1f678ea5f8c92121b6559a7ebf3495f2
Dash <eric@junglebet.com>
2026-02-23 23:54:32 -0500

    Inertia config

---
+++ config/inertia.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Inertia SSR
+    |--------------------------------------------------------------------------
+    |
+    | If you are not running a Node SSR server, keep this disabled.
+    |
+    */
+
+    'ssr' => [
+        'enabled' => env('INERTIA_SSR_ENABLED', false),
+
+        // Only used when enabled:
+        'url' => env('INERTIA_SSR_URL', 'http://127.0.0.1:13714'),
+
+        // Only used when enabled:
+        'bundle' => env('INERTIA_SSR_BUNDLE', base_path('bootstrap/ssr/ssr.js')),
+    ],
+
+];
